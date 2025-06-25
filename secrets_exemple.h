c1b0b0461f966b048a95dfdbe3f29a02fc5e2dc0
Christian Zufferey <christian@zufferey.com>
2025-06-25 09:45:43 +0200

    new: ajouté le fichier exemples pour les secrets

---
+++ secrets_exemple.h
@@ -0,0 +1,44 @@
+// zf240528.1859, 250625.0829
+// gestion des secrets pour ce projet
+// faire un :
+// cp secrets_exemple.h projet/secrets.h
+// et modifier secrets.h en conséquence
+
+// WIFI
+#define WIFI_SSID "xxx"
+#define WIFI_PASSWORD "xxx"
+
+#define WIFI_SSID1 "xxx"
+#define WIFI_PASSWORD1 "xxx"
+#define WIFI_SSID2 "xxx"
+#define WIFI_PASSWORD2 "xxx"
+#define WIFI_SSID3 "xxx"
+#define WIFI_PASSWORD3 "xxx"
+#define WIFI_SSID4 "xxx"
+#define WIFI_PASSWORD4 "xxx"
+#define WIFI_SSID5 "xxx"
+#define WIFI_PASSWORD5 "xxx"
+#define WIFI_SSID6 "xxx"
+#define WIFI_PASSWORD6 "xxx"
+#define WIFI_SSID7 "xxx"
+#define WIFI_PASSWORD7 "xxx"
+#define WIFI_SSID8 "xxx"
+#define WIFI_PASSWORD8 "xxx"
+#define WIFI_SSID9 "xxx"
+#define WIFI_PASSWORD9 "xxx"
+#define WIFI_SSID0 "xxx"
+#define WIFI_PASSWORD10 "xxx"
+
+// MQTT
+// #define BROKER_ADDR IPAddress(x,x,x,x)
+// #define BROKER_ADDR IPAddress(x,x,x,x)
+#define BROKER_ADDR "xxx"
+#define BROKER_USERNAME "xxx"
+#define BROKER_PASSWORD "xxx"
+
+// API REST
+#define apiServerName "https://xxx"
+#define apiToken "xxx"
+
+// OTA Arduino IDE
+#define ArduinoOtaPassword "xxx"
